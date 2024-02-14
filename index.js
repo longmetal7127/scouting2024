9151726006a47c745072fa61035e9d8ff09ac986
LongmetalAdmin <115046046+LongmetalAdmin@users.noreply.github.com>
2024-02-14 17:55:00 -0500

    added global id

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const getTodos = async () => {
 	<div class="team">
 	<div class="content">
 	<input id="edit" class="text" readonly="readonly" type="text" value= ${todo.todo}>
+  <label value=${todo.globalid}>
 	</div>
 	<div class="actions">
 	<button class="delete" onclick="deleteTodo(event, ${todo.id})">Delete</button>
