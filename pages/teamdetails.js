7a4d4768a649e7f141fc8a890e05093bc80310f9
LongmetalAdmin <115046046+LongmetalAdmin@users.noreply.github.com>
2024-02-28 19:43:20 -0500

    dfghj

--- pages/teamdetails.js
+++ pages/teamdetails.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     // Initialize Dexie and retrieve data based on the team ID from the URL parameter
     const urlParams = new URLSearchParams(window.location.search);
     const teamId = parseInt(urlParams.get('globalid'));
-    
+    console.log(`${teamId}`);
+
     const db = new Dexie("Team Tracking App");
     db.version(1).stores({
         teams: "++id, teamname, globalid"
@@ -12,7 +13,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const team = await db.teams.get(teamId);
     if (team) {
         // Display the team details on the page
-        const teamDetailsDiv = document.getElementById('teamname');
+        const teamDetailsDiv = document.getElementById('test');
         teamDetailsDiv.innerHTML = `${team.teamname}`;
     } else {
         console.log(`Team with ID ${teamId} not found.`);
