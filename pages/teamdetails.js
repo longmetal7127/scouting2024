75d94ebef03f582e5e88e7668f5224d5e0b73c79
jmurgolo <james.murgolo@gmail.com>
2024-03-01 14:07:44 -0500

    aaaaa

--- pages/teamdetails.js
+++ pages/teamdetails.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', async () => {
     try {
         const urlParams = new URLSearchParams(window.location.search);
-        const globalid = parseInt(urlParams.get('globalid'));
+        const globalid = parseInt(urlParams.get('globalid','teamnumber'));
         
         const db = new Dexie("Team Tracking App");
         db.version(1).stores({ teams: "++id, teamname, globalid" });
 
         const team = await db.teams.where('globalid').equals(globalid).first();
         if (team) {
-            const teamDetailsDiv = document.getElementById('teamname');
-            teamDetailsDiv.value = `${team.teamname}`;
+            const teamNameElm = document.getElementById('teamname');
+            teamNameElm.value = `${team.teamname}`;
+            const teamNumElm = document.getElementById('teamnumber');
+            teamNumElm.value = `${team.teamname}`;
         } else {
             console.log(`Team with ID ${globalid} not found.`);
         }
