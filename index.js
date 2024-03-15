6aa59d5ac89be2093d3c72ecbb770a49f008ff99
jmurgolo <james.murgolo@gmail.com>
2024-03-14 23:23:06 -0400

    sql

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const db = new Dexie("Team Tracking App");
 //trap was in there twice throwing errors so I removed the last one of them (2nd to last entry)
 
-db.version(2).stores({ 
-  teams: "++id, teamname, globalid, teamnumber, teamschool, alliancescore, moreinfo, startingpos, Leaveszone, scores1amp, scores1speaker, picksup, scores2amp, scores2speaker, preferredScoringMethod, preferredIntakeMethod, prefintake, spotlight, trap, alone, hangsWithAnother, attemptsSpotlight, coop", 
+db.version(3).stores({ 
+  teams: "++indexid, teamname, globalid, teamnumber, teamschool, alliancescore, moreinfo, startingpos, Leaveszone, scores1amp, scores1speaker, picksup, scores2amp, scores2speaker, preferredScoringMethod, preferredIntakeMethod, prefintake, spotlight, trap, alone, hangsWithAnother, attemptsSpotlight, coop", 
   matches: "++id, rank, matchnumber, count1, count2, count3, count4, count5, count6, count7, stage, hangs, harmony, otherinfo"
 });
   // Version numbers must be changed whenever database objects (schema) are edited? See "Modify Schema" in https://dexie.org/docs/Tutorial/Understanding-the-basics
